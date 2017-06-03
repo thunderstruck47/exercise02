78965073653a39a5b2abb8e99371277a94ba23ef
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2017-06-03 22:46:47 +0300

    added collector logic

--- collector.py
+++ collector.py
@@ -1,5 +1,32 @@
 from redis import Redis
 from stats import Store
+from threading import Thread
+
+class Collector(Thread):
+    def __init__(self, r, channels):
+        Thread.__init__(self)
+        self.redis = r
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(channels)
+        self.stats = Store()
+    
+    def work(self, item):
+        data = item['data'].decode()
+        #print(data.rsplit(' ', 2))
+        print(data)
+        #result = eval('self.stats.' + data)
+
+    def run(self):
+        for item in self.pubsub.listen():
+            #print(item)
+            if item['data'] == 1: pass
+            elif item['data'] == "KILL":
+                print(self, "unsubscribed and finished")
+                break
+            else:
+                self.work(item)
 
 if __name__ == '__main__':
-    pass
+    r = Redis()
+    c = Collector(r,['statistics'])
+    c.start()
